d28297c78e5df43f0e96f0718540d823d0110b0d
daria trip <daria.trip@outlook.com>
2021-03-10 08:34:22 +0200

    af3

---
+++ Matrici 3/Ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        /// <summary>
+        /// Parcurgerea in spirala
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //TODO
+
+        }
+    }
+}
---
+++ Matrici 3/Matrici 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        /// <summary>
+        /// zonele dintr-o matrice
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int n = 9;
+            int[,] a = new int[n, n];
+            for(int i=0;i<9;i++)
+            {
+                for (int j = 0;j< 9;j++)
+                {
+                    if ((i < j) && (i + j < n - 1))
+                        a[i, j] = 1;
+                    if ((i < j) && (i + j > n - 1))
+                        a[i, j] = 2;
+                    if ((i > j) && (i + j < n - 1))
+                        a[i, j] = 3;
+                    if ((i > j) && (i + j > n - 1))
+                        a[i, j] = 4;
+
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                    Console.Write(a[i, j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
